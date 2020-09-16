d05fc20dd8100b18b0950ddf0eda53207002e10a
Kamil Blanik <kamil.lukasz.blanik@gmail.com>
2020-09-17 00:56:09 +0200

    Initial commit

---
+++ src/main/java/it/arcade/hospital/config/WebSecurityConfiguration.java
@@ -0,0 +1,29 @@
+package it.arcade.hospital.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.
+                authorizeRequests()
+                    .antMatchers("/", "/index").permitAll()
+                    .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**");
+    }
+
+}
