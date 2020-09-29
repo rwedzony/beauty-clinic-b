5ea2b83dc4f789b804dac9c491fce70c908bbf13
Anna <annadrozdz33@gmail.com>
2020-09-29 19:47:24 +0200

    Offer_repository_and_Service_correction

--- src/main/java/it/arcade/hospital/offer/OfferRepository.java
+++ src/main/java/it/arcade/hospital/offer/OfferRepository.java
@@ -3,10 +3,10 @@ package it.arcade.hospital.offer;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 
-@RepositoryRestResource(collectionResourceRel = "offers", path = "offers")
+@Repository
 public interface OfferRepository extends JpaRepository<Offer, Long> {
 
 	List<Offer> findAllByDepartment(Department department);
--- src/main/java/it/arcade/hospital/offer/OfferService.java
+++ src/main/java/it/arcade/hospital/offer/OfferService.java
@@ -20,31 +20,14 @@ public class OfferService {
 	}
 	
 	public List<Offer> getAllOffersForDepartment(Department department) {
-		List<Offer> offers =  offerRepository.findAllByDepartment(department);
+		List<Offer> offers = offerRepository.findAllByDepartment(department);
 		return isEmpty(offers) ? new ArrayList<Offer>() : offers;
 	}
 	
 	public List<Offer> getAllOffersByName (String name) {
-		List<Offer> offers =   offerRepository.findAllByName(name);
+		List<Offer> offers = offerRepository.findAllByName(name);
 		return isEmpty(offers) ? new ArrayList<Offer>() : offers;
 	}
-	
-	public Offer saveOrUpdateOffer(Offer offer) {
-		if(offer.getId() != null) {
-			Optional<Offer> foundOffer = offerRepository.findById(offer.getId());
-			if(foundOffer.isPresent()) {
-				Offer updatedOffer = new Offer(foundOffer.get().getId(), offer.getName(), offer.getPrice(), offer.getDepartment());
-				offerRepository.save(updatedOffer);
-				return updatedOffer;
-			} else {
-				offer = offerRepository.save(offer);
-				return offer;
-			}
-		} else {
-			offer = offerRepository.save(offer);
-			return offer;
-		}
-	}
-	
+
 }
 
