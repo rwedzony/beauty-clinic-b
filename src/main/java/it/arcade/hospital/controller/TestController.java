112130d5d102f2ce5d19bf6de2afeef77750fca8
BarbarBarabasz <barbarski@interia.eu>
2020-11-10 21:32:05 +0100

    TestClass to return in JSON TestController to request mapping

---
+++ src/main/java/it/arcade/hospital/controller/TestController.java
@@ -0,0 +1,13 @@
+package it.arcade.hospital.controller;
+
+import it.arcade.hospital.testclass.TestClass;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public TestClass getTest(){
+        return new TestClass("helloWorld");
+    }
+}
