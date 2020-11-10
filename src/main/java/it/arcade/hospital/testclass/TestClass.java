fd9a7fd664b88285146701fa2d7036d3a5e51eff
BarbarBarabasz <barbarski@interia.eu>
2020-11-10 21:15:41 +0100

    TestClass to return in JSON

---
+++ src/main/java/it/arcade/hospital/testclass/TestClass.java
@@ -0,0 +1,21 @@
+package it.arcade.hospital.testclass;
+
+public class TestClass {
+
+    String test;
+
+    public TestClass() {
+    }
+
+    public TestClass(String test) {
+        this.test = test;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
