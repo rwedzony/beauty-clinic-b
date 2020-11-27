f46c4811b5f4f21866b073acdad7be6e0f0ce289
BarbarBarabasz <barbarski@interia.eu>
2020-11-27 16:30:06 +0100

    TestController to request mapping

--- src/main/java/it/arcade/hospital/controller/TestController.java
+++ src/main/java/it/arcade/hospital/controller/TestController.java
@@ -1,12 +1,11 @@
 package it.arcade.hospital.controller;
-import it.arcade.hospital.testclass.TestClass;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("/test")
-    public TestClass getTest(){
-        return new TestClass("helloWorld");
+    @GetMapping("/test")
+    public String getTest(){
+        return "helloWorld";
     }
 }
