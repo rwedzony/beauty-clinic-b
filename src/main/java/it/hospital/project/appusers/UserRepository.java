bacf7c2ecdc71980f8ccfd4dd0ccfd3d61cf7bb1
ilowski <wojtek-ilowski@wp.pl>
2021-04-15 16:46:31 +0200

    delete import of user

--- src/main/java/it/hospital/project/appusers/UserRepository.java
+++ src/main/java/it/hospital/project/appusers/UserRepository.java
@@ -1,6 +1,6 @@
 package it.hospital.project.appusers;
 
-import it.hospital.project.appusers.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
