02ffaaeef5b3628028b2be98aeeec1392e64cbbe
rwedzony <rwedzony@gmail.com>
2021-04-30 19:01:38 +0200

    remove security configuration

--- src/main/java/it/hospital/project/securityconfig/WebSecurityConfiguration.java
+++ src/main/java/it/hospital/project/securityconfig/WebSecurityConfiguration.java
@@ -1,29 +1,29 @@
-package it.hospital.project.securityconfig;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.
-                    authorizeRequests()
-                    .antMatchers("/**").permitAll()
-                    .anyRequest().authenticated();
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring()
-                .antMatchers("/resources/**", "/static/**");
-    }
-
-}
+//package it.hospital.project.securityconfig;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.
+//                    authorizeRequests()
+//                    .antMatchers("/**").permitAll()
+//                    .anyRequest().authenticated();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web
+//                .ignoring()
+//                .antMatchers("/resources/**", "/static/**");
+//    }
+//
+//}
