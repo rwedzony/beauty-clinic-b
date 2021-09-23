d3e926856142efb15f3ca0ad4b1a5df66032c389
rwedzony <rwedzony@gmail.com>
2021-09-23 20:21:47 +0200

    swagger configuration improvement

--- src/main/java/pl/rafsoftrw/beautyclinic/healthEndpoint/HealthEndpoint.java
+++ src/main/java/pl/rafsoftrw/beautyclinic/healthEndpoint/HealthEndpoint.java
@@ -2,17 +2,26 @@ package pl.rafsoftrw.beautyclinic.healthEndpoint;
 
 import java.util.Map;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "Health", description = "usługa sprawdzająca działanie aplikacji")
 @RestController
 @RequestMapping("/health")
 @CrossOrigin
 public class HealthEndpoint {
 
+    @ApiOperation(value = "HealthEndpoint to check if app is running")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK")
+    })
     @GetMapping("")
     public ResponseEntity<?> getTestData() {
         return ResponseEntity.ok(Map.of("status", "OK"));
