344b2df831c28379edbb2b2eed1253fe2068fdfc
Rafał Wędzony <r.wedzony@visiona.pl>
2021-12-07 18:28:54 +0100

    add Apointment and Specialty model

---
+++ src/main/java/pl/rafsoftrw/beautyclinic/appointment/Appointment.java
@@ -0,0 +1,29 @@
+/*
+ * This code is unpublished proprietary trade secret of
+ * Visiona Sp. z o.o., ul. Życzkowskiego 14, 31-864 Kraków, Poland.
+ *
+ * This code is protected under Act on Copyright and Related Rights
+ * and may be used only under the terms of license granted by
+ * Visiona Sp. z o.o., ul. Życzkowskiego 14, 31-864 Kraków, Poland.
+ *
+ * Above notice must be preserved in all copies of this code.
+ */
+
+package pl.rafsoftrw.beautyclinic.appointment;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "T_APPOINTMENTS")
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+}
