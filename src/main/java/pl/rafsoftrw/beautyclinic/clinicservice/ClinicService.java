9ed6858f4b5531cdb87e7a34b13af74cd2390538
rwedzony <rwedzony@gmail.com>
2021-12-08 21:31:20 +0100

    fill clinicservice entity with non relational fields

--- src/main/java/pl/rafsoftrw/beautyclinic/clinicservice/ClinicService.java
+++ src/main/java/pl/rafsoftrw/beautyclinic/clinicservice/ClinicService.java
@@ -17,6 +17,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "T_CLINIC_SERVICES")
@@ -26,4 +27,40 @@ public class ClinicService {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private Long id;
+
+    @Column(name = "ID")
+    private Integer price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClinicService that = (ClinicService) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ClinicService{" +
+                "id=" + id +
+                ", price=" + price +
+                '}';
+    }
 }
